aeee1f9923ad9a25f7e3f88752a962ed380259b3
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-07-09 01:42:22 +0530

    Basic data modelled

---
+++ src/models.rs
@@ -0,0 +1,24 @@
+use url::Url;
+use uuid::Uuid;
+use dotenv::dotenv;
+use std::env;
+
+
+}
+pub struct Sauce{
+	pub id: Uuid,
+	pub url: Url,
+	pub short_url: Url,
+}
+
+
+
+impl Sauce {
+	pub fn new(url: String, short_url: String) -> Self {
+		Self {
+			id: Uuid::new_v4(),
+			url: Url::parse(&url).expect("Invalid URL provided!").to_string().parse().unwrap(),
+			short_url: Url::parse(&short_url).expect("Trouble parsing short URL!").to_string().parse().unwrap(),
+		}
+	}
+}
