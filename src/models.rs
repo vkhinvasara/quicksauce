94e9d1ea58bf8ef8577cf2572ca098f6901c02f6
vkhinvasara <vaibhavkhinvasara@gmail.com>
2024-07-09 13:45:23 +0530

    models updated and util to shorten url added

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,6 @@
 use url::Url;
 use uuid::Uuid;
-use dotenv::dotenv;
-use std::env;
 
-
-}
 pub struct Sauce{
 	pub id: Uuid,
 	pub url: Url,
@@ -14,7 +10,7 @@ pub struct Sauce{
 
 
 impl Sauce {
-	pub fn new(url: String, short_url: String) -> Self {
+	pub fn new(url: &str, short_url: &str) -> Self {
 		Self {
 			id: Uuid::new_v4(),
 			url: Url::parse(&url).expect("Invalid URL provided!").to_string().parse().unwrap(),
---
+++ src/utils.rs
@@ -0,0 +1,22 @@
+use base64::{engine::general_purpose::STANDARD, Engine};
+use sha2::{Sha256, Digest};
+use dotenv::dotenv;
+
+/// Shortens a URL by hashing it and encoding the hash in base64.
+/// Returns the first 5 characters of the base64-encoded hash.
+/// Note: This method increases the risk of hash collisions.
+
+
+const SHORT_URL_LENGTH: usize = 5;
+
+fn shorten_url(url: &str) -> String {
+	let mut hasher = Sha256::new();
+	hasher.update(url);
+	let result = hasher.finalize();
+	let mut encoded = String::new();
+
+	STANDARD.encode_string(&result, &mut encoded);
+	let code = encoded[0..SHORT_URL_LENGTH].to_string();
+	dotenv().ok();
+	dotenv::var("BASE_URL").unwrap() + &code
+}
